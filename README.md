d02cb007f412e2c3b86505aac7bf42f94a5beeee
house_young <houseashley07@hotmail.com>
2017-07-01 16:45:13 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Varant_Logic_Framework
 目前处于开发阶段的变值测量体系
+# BitFilter
+用于从一个整条的二进制序列当中抽取某种特征的序列
+# MainFrame
+变值体系的核心计算方法
+# SeqOperator
+针对序列的操作方法
--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 
 from MainFrame import Segmentor,VLSequence,BitFilter
 from MainFrame.sort import HeapSort
+from Visualize import VLVisualize
 import numpy as np
 
 
@@ -18,7 +19,7 @@ def main():
     """
     p_array = []
     q_array = []
-    input_str = open("./TestData/test.bin.enc.char","rb").read(1000000)
+    input_str = open("./TestData/test.bin.enc.char", "rb").read(1000000)
     segment_size = 128
     coordinates = Segmentor.segmentor(input_str=input_str, segment_size=segment_size,offset=segment_size)
     for coordinate in coordinates:
@@ -28,9 +29,25 @@ def main():
     matrix,xedges,yedges = np.histogram2d(p_array,q_array,bins=[segment_size,segment_size/2],range=[[0,segment_size],[0,segment_size/2]])
 
     result = BitFilter.max_2d_filter(input_str,matrix,segment_size,segment_size)
-    
-    output_file = open("./TestData/filter.char","wb")
-    output_file.write(result[1])
+    p_array = []
+    q_array = []
+
+    # VLVisualize.vl_plot1d(p_array,q_array,m=segment_size,mod="p")
+    p_count_max = None
+    for i in xrange(0,128):
+        shifted_str = VLSequence.string_right_shift(result[1],i)
+        coordinates = Segmentor.segmentor(input_str=shifted_str, segment_size=segment_size,offset=segment_size)
+        for coordinate in coordinates:
+            p, q = VLSequence.p_q_count(shifted_str[coordinate[0]:coordinate[1]+1])
+            p_array.append(p)
+            q_array.append(q)
+        temp = VLVisualize.vl_plot1d(p_array,q_array,m=segment_size,mod="p",ylim=p_count_max,rs=i)
+        if i == 0:
+            p_count_max = temp
+        # VLVisualize.vl_plot1d(p_array,q_array,m=segment_size,mod="q",rs=i)
+        p_array = []
+        q_array = []
+    # output_file = open("./TestData/filter_xor.char","wb")
 
 if __name__ == '__main__':
     main()
