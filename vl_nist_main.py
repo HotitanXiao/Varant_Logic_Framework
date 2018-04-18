f3041b1c32ca54e6987d4fa0f331fcd8a8a9d057
house_young <houseashley07@hotmail.com>
2018-04-19 00:22:41 +0800

    vl_nist files

--- vl_nist_main.py
+++ vl_nist_main.py
@@ -42,7 +42,7 @@ def process():
 
 # m_set = [1024,1500]
 # length_set = [-1]
-m_set = [200,201,256]
+m_set = [500]
 length_set = [-1]
 def go(basepath="",filename="",read_length=length_set[0]):
     runs_p_value_array = []
@@ -86,10 +86,11 @@ def go(basepath="",filename="",read_length=length_set[0]):
 
 
 if __name__ == '__main__':
-    file_list = list_file(local_settings.getTestDataPath()+"/2018-03/AES/") 
-    basepath = local_settings.getTestDataPath()+"/2018-03/AES/"
-
-    for file_name in file_list: 
-        print file_name
-        for read_length in length_set:
-            go(basepath,file_name,read_length)
+    target_path_set = ["Quantum/"]
+    for t in target_path_set:
+        file_list = list_file(local_settings.getTestDataPath()+"/2018-03/"+t) 
+        basepath = local_settings.getTestDataPath()+"/2018-03/"+t
+        for file_name in file_list: 
+            print file_name
+            for read_length in length_set:
+                go(basepath,file_name,read_length)
